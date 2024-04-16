2f4536d43a81823876e0d218ff7214cef814baab
wooni97 <jhw970714@gmail.com>
2024-04-16 09:46:15 +0900

    feat: @Inject 어노테이션 사용이 가능한 DI 프레임워크 구현
    - Class에 대한 빈 인스턴스를 생성하는 메서드 instantiateClass()
    - Constructor에 대한 빈 인스턴스를 생성하는 메서드 instantiateConstructor()
    - 재귀를 실행하는 방식으로 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -43,5 +43,53 @@ public class BeanFactory {
     * 다른 빈과 의존관계를 가지지 않는 빈을 찾아 인스턴스를 생성할 때까지 재귀를 실행하는 방식으로 구현한다.
     * */
 
+    /*
+    * Class에 대한 빈 인스턴스를 생성하는 메소드
+    * 재귀함수의 시작은 instantiateClass()에서 시작한다.
+    * @Inject 어노테이션이 설정 되어있는 생성자가 존재하면 instantiateConstructor() 메소드를 통해 인스턴스를 생성하고,
+    * 존재하지 않을 경우 기본 생성자로 인스턴스를 생성한다.
+    * */
+    private Object instantiateClass(Class<?> clazz) {
+        Object bean = beans.get(clazz);
+        if (bean != null) {
+            return bean;
+        }
+
+        Constructor<?> injectedConstructor
+                = BeanFactoryUtils.getInjectedConstructor(clazz);
+
+        if (injectedConstructor == null) {
+            bean = BeanUtils.instantiate(clazz);
+            beans.put(clazz, bean);
+            return bean;
+        }
+
+        bean = instantiateConstructor(injectedConstructor);
+        beans.put(clazz, bean);
+        return bean;
+    }
+
+    /*
+    * Constuctor에 대한 빈 인스턴스를 생성하는 메소드
+    * 생성자의 인자로 전달할 빈이 생성되어 Map<Class<?>, Object>에 이미 존재하면 해당 빈을 활용하고
+    * 존재하지 않을 경우 instantiateClass() 메소드를 통해 빈을 생성한다.
+    * */
+    private Object instantiateConstructor(Constructor<?> constructor) {
+        // getParameterTypes() : 리플렉션에서 사용되는 메소드로, 생성자의 매개변수 타입을 가져오는 데 사용한다.
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        List<Object> args = Lists.newArrayList();
+
+        for (Class<?> clazz : parameterTypes) {
+            Class<?> concreteClass =
+                    BeanFactoryUtils.findConcreteClass(clazz, preInstantiateBeans);
+            Object bean = beans.get(concreteClass);
+            if (bean == null) {
+                bean = instantiateClass(concreteClass);
+            }
+            args.add(bean);
+        }
+        return BeanUtils.instantiateClass(constructor, args.toArray());
+    }
+
     }
 }
