5727ec7713e5a416fe242f15eac52e9cf4a72866
wooni97 <jhw970714@gmail.com>
2024-04-16 09:43:57 +0900

    feat: @Inject 어노테이션 사용이 가능한 DI 프레임워크 구현
    - Class에 대한 빈 인스턴스를 생성하는 메서드 instantiateClass()
    - Constructor에 대한 빈 인스턴스를 생성하는 메서드 instantiateConstructor()
    - 재귀를 실행하는 방식으로 구현

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,22 +1,28 @@
 package core.di.factory;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.Lists;
+import core.annotation.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Maps;
+import org.springframework.beans.BeanUtils;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private Set<Class<?>> preInstantiateBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiateBeans) {
+        this.preInstantiateBeans = preInstantiateBeans;
     }
 
     @SuppressWarnings("unchecked")
@@ -25,6 +31,17 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        for (Class<?> clazz : preInstantiateBeans) {
+            instantiateClass(clazz);
+        }
+    }
+
+    /*
+    * @Inject 어노테이션이 설정되어있는 생성자를 통해서 빈을 생성
+    * 그런데 이 생성자의 인자로 전달할 빈도 다른 빈과 의존관계가 있다.
+    * 이와 같이 꼬리에 꼬리를 물고 빈 간의 의존관계가 발생할 수 있다.
+    * 다른 빈과 의존관계를 가지지 않는 빈을 찾아 인스턴스를 생성할 때까지 재귀를 실행하는 방식으로 구현한다.
+    * */
 
     }
 }
